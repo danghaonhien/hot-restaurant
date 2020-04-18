54633e3d1b1f1a2ab23fd957e23c717181bb6eba
Nhien Dang <danghaonhien@gmail.com>
2020-04-18 11:02:54 -0700

    switch src folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-ReactDOM.render(<App />,document.getElementById('root'));   
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
